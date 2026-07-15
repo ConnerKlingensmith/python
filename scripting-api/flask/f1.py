8b621d4589acbfa210e1a5ad95423f0eae807ec4
ConnerKlingensmith <connerkling22@gmail.com>
2026-07-15 19:35:25 +0000

    add flask2.py

--- scripting-api/flask/f1.py
+++ scripting-api/flask/f1.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app) # enables flask CORS
+
+@app.before_request
+def log_request():    # Shows the method and the path requested by client in the server logs 
+    print("Method: ", request.method)
+    print("Path: ", request.path)    
 
 @app.route("/")
 def home():
@@ -14,6 +22,16 @@ def about():
 def server():
     return "servername: webserver1"
 
+# Error Handling
+@app.route("/switches/<id>")
+def switches(id):
+    abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({
+        "error": "page not found"
+        }),404
 
 if __name__ == "__main__":
     app.run(debug=True, host="172.17.0.1", port=8080)
