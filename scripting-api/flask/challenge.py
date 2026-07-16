56708fae2a4a99be718e7d4d620448ca57fc969f
ConnerKlingensmith <connerkling22@gmail.com>
2026-07-16 13:19:47 +0000

    my challenge flask

--- scripting-api/flask/challenge.py
+++ scripting-api/flask/challenge.py
@@ -6,5 +6,14 @@ import platform
 
 app = Flask(__name__)
 
+# In memory, lost on restart
+network_data - []
 
+@app.before_request
+def log_request():
+    print("Method: ", request.method)
+    print("Path: ", request.path)
+    print("Time: ", request.time)
+
+@app.route
 
